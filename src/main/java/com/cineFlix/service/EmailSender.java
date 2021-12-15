4cabadb82c27e4d6e159788e58af73f82a31665d
rella-raffick <rellaraffickitvec@gmail.com>
2021-12-15 13:01:56 +0530

    Email & PDF Added in Service Package

---
+++ src/main/java/com/cineFlix/service/EmailSender.java
@@ -0,0 +1,64 @@
+package com.cineFlix.service;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Multipart;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+
+public class EmailSender {
+    public static void main(String[] args) {
+        String to = "rellaraffickitvec@gmail.com";
+        String from = "cineflixtickets@gmail.com";
+        String host = "smtp.gmail.com";
+        Properties properties = System.getProperties();
+
+        properties.put("mail.smtp.host", host);
+        properties.put("mail.smtp.port", "465");
+        properties.put("mail.smtp.ssl.enable", "true");
+        properties.put("mail.smtp.auth", "true");
+
+        Session session = Session.getInstance(properties, new javax.mail.Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication("cineflixtickets@gmail.com", "Yamaha14.");
+            }
+        });
+
+        try {
+            MimeMessage message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(from));
+            message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+            message.setSubject("Your Booked Ticket in Cineflix");
+            Multipart multipart = new MimeMultipart();
+            MimeBodyPart attachmentPart = new MimeBodyPart();
+            MimeBodyPart textPart = new MimeBodyPart();
+
+            try {
+                File f = new File("ticket.pdf");
+
+                attachmentPart.attachFile(f);
+                textPart.setText("Catch up early & Have Fun!");
+                multipart.addBodyPart(textPart);
+                multipart.addBodyPart(attachmentPart);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            message.setContent(multipart);
+            System.out.println("sending...");
+            Transport.send(message);
+            System.out.println("Sent message successfully....");
+        } catch (MessagingException mex) {
+            mex.printStackTrace();
+        }
+
+    }
+}
