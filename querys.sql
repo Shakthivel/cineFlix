7cbf2987d7a6558107d40bac042d2f8c4954947f
Rella Raffick <mrellamr@presidio.com>
2024-09-10 12:59:55 +0530

    Updated Query and Properties

--- querys.sql
+++ querys.sql
@@ -1,2 +1,2 @@
-insert into admin values(1234,"aaaa");
-insert into theatre values(1234,"aaaa","","");
+INSERT INTO user (name, password, email, number, address)
+VALUES ('John Doe', 'password123', 'john.doe@example.com', '1234567890', '123 Street, City, Country');
